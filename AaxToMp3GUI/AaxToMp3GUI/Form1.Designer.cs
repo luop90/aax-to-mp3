c8e0e981db02302a2174eb9d05a66b53c0238ba3
Luop <luop90@gmx.com>
2017-06-30 08:29:26 -0600

    Translated buttons

--- AaxToMp3GUI/AaxToMp3GUI/Form1.Designer.cs
+++ AaxToMp3GUI/AaxToMp3GUI/Form1.Designer.cs
@@ -46,7 +46,7 @@
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(87, 23);
             this.button1.TabIndex = 0;
-            this.button1.Text = "Durchsuchen";
+            this.button1.Text = "Open File";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
@@ -57,7 +57,7 @@
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(87, 23);
             this.button2.TabIndex = 1;
-            this.button2.Text = "Umwandeln";
+            this.button2.Text = "Convert";
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
